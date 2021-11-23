4779c7ec9cd1cea67076e4444c838cc017e94784
pramodgit13 <94871952+pramodgit13@users.noreply.github.com>
2021-11-23 16:08:04 -0600

    My first commit

---
+++ src/main/java/com/example/DockerJenkinsSample/DockerJenkinsSampleApplication.java
@@ -0,0 +1,22 @@
+package com.example.DockerJenkinsSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DockerJenkinsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsSampleApplication.class, args);
+	}
+	@RestController
+	public class Welcome {
+
+		@GetMapping
+		public String HomePage() {
+			return "Hard work!";
+		}
+	}
+}
---
+++ src/test/java/com/example/DockerJenkinsSample/DockerJenkinsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DockerJenkinsSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
